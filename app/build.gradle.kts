8e955795638728e49385c163c52cf1fc2d2c901c
mikucat0309 <47076023+mikucat0309@users.noreply.github.com>
2022-07-23 18:47:14 +0800

    fix: 移除 忽略相容性檢查 flag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,6 @@ android {
         jvmTarget = "1.8"
         freeCompilerArgs = freeCompilerArgs + listOf(
             "-opt-in=kotlin.RequiresOptIn",
-            "-P",
-            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true",
         )
     }
     buildFeatures {
