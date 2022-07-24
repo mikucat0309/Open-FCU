0e5390fdc1fa2a9803f1b53b9afaf4c247a2f292
mikucat0309 <47076023+mikucat0309@users.noreply.github.com>
2022-07-24 20:28:48 +0800

    chore: Add kotest 5.3.2 & mockk 1.12.4 remove mockative & junit5 gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,10 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
-    id("kotlin-kapt")
+    kotlin("kapt")
     id("dagger.hilt.android.plugin")
-    id("de.mannodermaus.android-junit5")
-    id("com.google.devtools.ksp")
 }
 
 android {
@@ -50,6 +48,11 @@ android {
             "-opt-in=kotlin.RequiresOptIn",
         )
     }
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform()
+        }
+    }
     buildFeatures {
         compose = true
     }
@@ -68,16 +71,6 @@ android {
     }
 }
 
-kotlin {
-    sourceSets {
-        val test by getting {
-            dependencies {
-                implementation("io.mockative:mockative:1.2.3")
-            }
-        }
-    }
-}
-
 hilt {
     enableAggregatingTask = true
 }
@@ -87,6 +80,7 @@ val ktorVersion = "2.0.3"
 val lifecycleVersion = "2.5.0"
 val navVersion = "2.5.0"
 val hiltVersion = "2.43"
+val coroutineVersion = "1.6.4"
 
 dependencies {
     implementation("androidx.core:core-ktx:1.8.0")
@@ -104,6 +98,8 @@ dependencies {
     api("androidx.navigation:navigation-fragment-ktx:$navVersion")
 
     implementation("com.google.dagger:hilt-android:$hiltVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
@@ -113,21 +109,23 @@ dependencies {
 
     implementation("io.github.g0dkar:qrcode-kotlin-android:3.1.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutineVersion")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
-    configurations
-        .filter { it.name.startsWith("ksp") && it.name.contains("Test") }
-        .forEach {
-            add(it.name, "io.mockative:mockative-processor:1.2.3")
-        }
+    testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.3.2")
+
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk-agent-jvm:1.12.4")
 
 //    testImplementation("junit:junit:4.13.2")
 //    androidTestImplementation("androidx.test.ext:junit:1.1.3")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 buildscript {
     dependencies {
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43")
     }
 }
@@ -10,7 +9,6 @@ plugins {
     id("com.android.library") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6" apply false
 }
 
 tasks.register("clean", Delete::class) {
