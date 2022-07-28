4849ee0890fd06518f30debd80e95d6443e58b54
mikucat0309 <47076023+mikucat0309@users.noreply.github.com>
2022-07-28 11:45:50 +0800

    refactor: 將 CourseLazyColumnView 拆解成多個 composable

--- app/src/main/java/at/mikuc/openfcu/view/CourseSearchResultView.kt
+++ app/src/main/java/at/mikuc/openfcu/view/CourseSearchResultView.kt
@@ -29,86 +29,105 @@ private fun CourseLazyColumnView(courses: List<Course>) {
             .background(MaterialTheme.colors.background)
             .padding(16.dp)
     ) {
-        items(courses) { course ->
-            course.run {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(64.dp)
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                ) {
-                    Box(
-                        modifier = Modifier.weight(2f, fill = true)
-                    ) {
-                        Text(
-                            text = name,
-                            maxLines = 2,
-                            overflow = TextOverflow.Ellipsis,
-                            style = MaterialTheme.typography.subtitle1,
-                        )
-                    }
-                    Spacer(Modifier.width(8.dp))
-                    Box(
-                        modifier = Modifier.weight(1f, fill = true)
-                    ) {
-                        Text(
-                            text = teacher,
-                            maxLines = 2,
-                            overflow = TextOverflow.Ellipsis,
-                            style = MaterialTheme.typography.body1,
-                        )
-                    }
-                    Spacer(Modifier.width(8.dp))
-                    Column(modifier = Modifier.weight(1f)) {
-                        periods.forEach { period ->
-                            period.run {
-                                val rangeStr =
-                                    if (range.first == range.last) range.first.toString()
-                                    else "${range.first}-${range.last}"
-                                Text(
-                                    text = "(${day2str[day] ?: "N/A"}) $rangeStr",
-                                    maxLines = 1,
-                                    softWrap = false,
-                                    overflow = TextOverflow.Clip,
-                                    style = MaterialTheme.typography.body1
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        items(courses) { CourseRow(it) }
     }
+}
+
+@Composable
+private fun CourseRow(course: Course) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        CourseNameField(course, Modifier.weight(2f, fill = true))
+        Spacer(Modifier.width(8.dp))
+        TeacherField(course, Modifier.weight(1f, fill = true))
+        Spacer(Modifier.width(8.dp))
+        PeriodField(course, modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+private fun CourseNameField(course: Course, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Text(
+            text = course.name,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
+}
+
+@Composable
+private fun TeacherField(course: Course, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Text(
+            text = course.teacher,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.body1,
+        )
+    }
+}
+
+@Composable
+private fun PeriodField(course: Course, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        course.periods.forEach { PeriodLine(it) }
+    }
+}
+
+@Composable
+private fun PeriodLine(period: Period) {
+    val rangeStr =
+        if (period.range.first == period.range.last) period.range.first.toString()
+        else "${period.range.first}-${period.range.last}"
+    Text(
+        text = "(${day2str[period.day] ?: "N/A"}) $rangeStr",
+        maxLines = 1,
+        softWrap = false,
+        overflow = TextOverflow.Clip,
+        style = MaterialTheme.typography.body1
+    )
 }
 
 @Preview(showBackground = true, widthDp = 400)
 @Composable
 fun CourseSearchResultPreview() {
-    CourseLazyColumnView(courses = listOf(
-        Course(
-            name = "中文思辨與表達中文思辨與表達(一)",
-            id = "ID",
-            code = 1,
-            teacher = "王小明、王中明",
-            periods = listOf(
-                Period(4, (9..14), "布宜諾斯艾利斯是個很長的地名"),
-                Period(5, (12..14), "躲貓貓社辦")
-            ),
-            credit = 3,
-            opener = Opener(
-                "OP_NAME",
-                "acaID",
-                "DEPART_ID",
-                'A',
-                'B',
-                'C'
-            ),
-            openNum = 70,
-            acceptNum = 70,
-            remark = "This is a REMARK This is a REMARK This is a REMARK This is a REMARK"
+    CourseLazyColumnView(
+        courses = listOf(
+            Course(
+                name = "中文思辨與表達中文思辨與表達(一)",
+                id = "ID",
+                code = 1,
+                teacher = "王小明、王中明",
+                periods = listOf(
+                    Period(4, (9..14), "布宜諾斯艾利斯是個很長的地名"),
+                    Period(5, (12..14), "躲貓貓社辦")
+                ),
+                credit = 3,
+                opener = Opener(
+                    "OP_NAME",
+                    "acaID",
+                    "DEPART_ID",
+                    'A',
+                    'B',
+                    'C'
+                ),
+                openNum = 70,
+                acceptNum = 70,
+                remark = "This is a REMARK This is a REMARK This is a REMARK This is a REMARK"
+            )
         )
-    ))
+    )
 }
 
 val day2str = mapOf(
