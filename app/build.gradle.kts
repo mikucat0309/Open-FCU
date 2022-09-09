df7f3e9349304b15dc4333823986b6e538cd541d
mikucat0309 <47076023+mikucat0309@users.noreply.github.com>
2022-09-09 18:39:05 +0800

    chore: 0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "0.5.1"
+        versionName = "0.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
